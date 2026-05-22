2a94a23c3f8cf77634a707702d34ff3758dd9af7
Gabriela Alves <gabriela.conceicao@eaportal.org>
2026-05-22 15:04:04 -0300

    corrigindo erro da calculadora, commit final

--- script.js
+++ script.js
@@ -71,18 +71,18 @@ botaoLimpar.addEventListener("click", limpar)
 function limpar() {
     contaInput.value = ""
 
-    botoesBorgeta.forEach(botao => {
+    botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
     })
 
     gorjetaInput.value = ""
     pessoasInput.value = ""
 
-    document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0,00"
-    document.querySelector(".valor-total > strong").innerHTML = "R$ 0,00"
+    document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
+    document.querySelector(".total > strong").innerHTML = "R$ 0.00"
 
     conta = 0
     porcentagem = 0
     pessoas = 0
-    
+
 }
